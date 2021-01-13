4cbad19d549b08bb5c5412f82871b434312b0ade
azabraao <az.abraao@gmail.com>
2021-01-13 16:17:05 -0300

    make countdown works

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+//react/Countdown.tsx
+import React, { useState } from "react";
+import { TimeSplit } from "./typings/global";
+import { tick, getTwoDaysFromNow } from "./utils/time";
 
 interface CountdownProps {
   targetDate: string;
 }
 
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
+
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate,
+  targetDate = DEFAULT_TARGET_DATE,
 }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  });
+
+  tick(targetDate, setTime);
+
   return (
     <div>
-      <h1>Countdown: {targetDate}</h1>
+      <h1>{`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</h1>
     </div>
   );
 };
@@ -23,7 +36,7 @@ Countdown.schema = {
       title: "Final date",
       description: "Final date used in the countdown",
       type: "string",
-      default: null,
+      default: "02/02/2021",
     },
   },
 };
